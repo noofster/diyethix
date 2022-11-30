1094a53bc321951e4e813c44e7381238fd5f14c6
noofster <77473995+noofster@users.noreply.github.com>
2022-11-29 20:05:40 -0500

    some more sound

--- pointscore.gd
+++ pointscore.gd
@@ -12,6 +12,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	if global_position.x<50:
+		dir = Vector2(1,0)
+	if global_position.x>800:
+		dir = Vector2(-1,0)
 	global_position += dir*150*delta
 	if(int($Label.text) >= 1000):
 		$Label.add_color_override("font_color", Color(rand_range(.5,1),rand_range(0,1),rand_range(.5,1),1))
